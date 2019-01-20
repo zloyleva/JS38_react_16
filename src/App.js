cf390f5eab9c7eedbf736df95ac9cbbdddcd8dd4
ZloyLeva <zloyleva@gmail.com>
2019-01-20 16:24:18 +0200

    pass data to Header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
   render() {
     return (
         <Fragment>
-            <Header />
+            <Header
+              title="The Rick and Morty API"
+              sub_title="Hey, did you ever want to hold a Terry fold?"
+              sub_title_hidden="&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;"
+            />
             <HeroList />
         </Fragment>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './style.css'
 
 
-function Header() {
-
+function Header(props) {
+    const {title,sub_title,sub_title_hidden} = props;
     return(
         <section className="Header ">
-            <h1 className="Home__Title">The Rick and Morty API</h1>
+            <h1 className="Home__Title">{title}</h1>
             <div className="Home__SubTitleWrapper">
-                <h2 className="Home__SubTitle">Hey, did you ever want to hold a Terry fold?</h2>
-                <h2 className="Home__HiddenSubTitle">&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;</h2>
+                <h2 className="Home__SubTitle">{sub_title}</h2>
+                <h2 className="Home__HiddenSubTitle">{sub_title_hidden}</h2>
             </div>
         </section>
     )

7b9dc414fe46805bd4b6eb36c3e0fe1a6803e6df
ZloyLeva <zloyleva@gmail.com>
2019-01-20 18:40:24 +0200

    install Router and create main rout

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 import Header from "./components/Header";
 import HeroList from "./components/HerosList";
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
-        <Fragment>
-            <Header
-              title="The Rick and Morty API"
-              sub_title="Hey, did you ever want to hold a Terry fold?"
-              sub_title_hidden="&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;"
-            />
-            <HeroList />
-        </Fragment>
+        <Router>
+            <Fragment>
+                <Header
+                    title="The Rick and Morty API"
+                    sub_title="Hey, did you ever want to hold a Terry fold?"
+                    sub_title_hidden="&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;"
+                />
+                <Route path="/" component={HeroList}/>
+            </Fragment>
+        </Router>
     );
   }
 }

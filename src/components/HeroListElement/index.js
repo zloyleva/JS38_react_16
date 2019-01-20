7217f184258d08c3c2a8c48a60ea24c119902412
ZloyLeva <zloyleva@gmail.com>
2019-01-20 17:29:03 +0200

    create HeroListElement

---
+++ src/components/HeroListElement/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function HeroListElement(props) {
+
+    return(
+        <article className="CharacterCard__Wrapper">
+            <div data="card header" className="CharacterCard__ImgWrapper">
+                <div className="card-image">
+                    <img src="https://rickandmortyapi.com/api/character/avatar/65.jpeg"
+                         alt="Chris"/>
+                </div>
+                <div className="CharacterCard__Title">
+                    <h2 className="CharacterCard__Name">Chris</h2>
+                    <p className="CharacterCard__Description">id: 65 - created a year ago</p>
+                </div>
+            </div>
+            <div data="card info" className="CharacterCard__InfoWrapper">
+                <div className="CharacterCard__TextWrapper"><span>STATUS</span><p>Alive</p>
+                </div>
+                <div className="CharacterCard__TextWrapper"><span>SPECIES</span><p>Humanoid,
+                    Microverse inhabitant</p></div>
+                <div className="CharacterCard__TextWrapper"><span>GENDER</span><p>Male</p>
+                </div>
+                <div className="CharacterCard__TextWrapper"><span>ORIGIN</span><p>Rick's
+                    Battery Microverse</p></div>
+                <div className="CharacterCard__TextWrapper"><span>LAST LOCATION</span>
+                    <p>Rick's Battery Microverse</p></div>
+            </div>
+        </article>
+    )
+}
+
+export default HeroListElement;
--- src/components/HerosList/index.js
+++ src/components/HerosList/index.js
@@ -1,65 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.css';
+import HeroListElement from "../HeroListElement";
 
-const HeroList = () => {
+class HeroList extends Component {
 
-    return(
-        <section className="Home__ShowcaseWrapper">
-            <div className="Home__ShowcaseInner">
+    render() {
+        return(
+            <section className="Home__ShowcaseWrapper">
+                <div className="Home__ShowcaseInner">
 
-                <article className="CharacterCard__Wrapper">
-                    <div data="card header" className="CharacterCard__ImgWrapper">
-                        <div className="card-image">
-                            <img src="https://rickandmortyapi.com/api/character/avatar/65.jpeg"
-                                                         alt="Chris"/>
-                        </div>
-                        <div className="CharacterCard__Title">
-                            <h2 className="CharacterCard__Name">Chris</h2>
-                            <p className="CharacterCard__Description">id: 65 - created a year ago</p>
-                        </div>
-                    </div>
-                    <div data="card info" className="CharacterCard__InfoWrapper">
-                        <div className="CharacterCard__TextWrapper"><span>STATUS</span><p>Alive</p>
-                        </div>
-                        <div className="CharacterCard__TextWrapper"><span>SPECIES</span><p>Humanoid,
-                            Microverse inhabitant</p></div>
-                        <div className="CharacterCard__TextWrapper"><span>GENDER</span><p>Male</p>
-                        </div>
-                        <div className="CharacterCard__TextWrapper"><span>ORIGIN</span><p>Rick's
-                            Battery Microverse</p></div>
-                        <div className="CharacterCard__TextWrapper"><span>LAST LOCATION</span>
-                            <p>Rick's Battery Microverse</p></div>
-                    </div>
-                </article>
+                    <HeroListElement />
+                    <HeroListElement />
+                    <HeroListElement />
+                    <HeroListElement />
 
-                <article className="CharacterCard__Wrapper">
-                    <div data="card header" className="CharacterCard__ImgWrapper">
-                        <div className="card-image">
-                            <img src="https://rickandmortyapi.com/api/character/avatar/65.jpeg"
-                                 alt="Chris"/>
-                        </div>
-                        <div className="CharacterCard__Title">
-                            <h2 className="CharacterCard__Name">Chris</h2>
-                            <p className="CharacterCard__Description">id: 65 - created a year ago</p>
-                        </div>
-                    </div>
-                    <div data="card info" className="CharacterCard__InfoWrapper">
-                        <div className="CharacterCard__TextWrapper"><span>STATUS</span><p>Alive</p>
-                        </div>
-                        <div className="CharacterCard__TextWrapper"><span>SPECIES</span><p>Humanoid,
-                            Microverse inhabitant</p></div>
-                        <div className="CharacterCard__TextWrapper"><span>GENDER</span><p>Male</p>
-                        </div>
-                        <div className="CharacterCard__TextWrapper"><span>ORIGIN</span><p>Rick's
-                            Battery Microverse</p></div>
-                        <div className="CharacterCard__TextWrapper"><span>LAST LOCATION</span>
-                            <p>Rick's Battery Microverse</p></div>
-                    </div>
-                </article>
-
-            </div>
-        </section>
-    )
-};
+                </div>
+            </section>
+        )
+    }
+}
 
 export default HeroList;

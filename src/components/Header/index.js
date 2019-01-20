fd84ea2dc6e22493581ed18dfe882122b16763c6
ZloyLeva <zloyleva@gmail.com>
2019-01-20 16:19:42 +0200

    finish Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import './style.css'
 
 
 function Header() {
 
     return(
-        <div className="">
-            <h1>The Rick and Morty API</h1>
-            <div>
-                <h2>Hey, did you ever want to hold a Terry fold?</h2>
-                <h2>&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;</h2>
+        <section className="Header ">
+            <h1 className="Home__Title">The Rick and Morty API</h1>
+            <div className="Home__SubTitleWrapper">
+                <h2 className="Home__SubTitle">Hey, did you ever want to hold a Terry fold?</h2>
+                <h2 className="Home__HiddenSubTitle">&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;</h2>
             </div>
-        </div>
+        </section>
     )
 }
 

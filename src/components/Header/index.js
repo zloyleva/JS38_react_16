e6f464df621583574e4cdb481ce1cb8716ff8b36
ZloyLeva <zloyleva@gmail.com>
2019-01-20 16:04:13 +0200

    add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Header() {
+
+    return(
+        <div className="">
+            <h1>The Rick and Morty API</h1>
+            <div>
+                <h2>Hey, did you ever want to hold a Terry fold?</h2>
+                <h2>&nbsp;&nbsp;&nbsp;I got one right here, grab my terry flap&nbsp;&nbsp;&nbsp;</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

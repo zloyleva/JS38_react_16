0472632860ba3df9216ddbdaf08e9a2f4c3bba76
ZloyLeva <zloyleva@gmail.com>
2019-01-20 17:27:12 +0200

    add css to HerosList

--- src/components/HerosList/index.js
+++ src/components/HerosList/index.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import './style.css';
 
 const HeroList = () => {
 
     return(
-        <div className="">
-            HeroList
-        </div>
+        <section className="Home__ShowcaseWrapper">
+            <div className="Home__ShowcaseInner">
+
+                <article className="CharacterCard__Wrapper">
+                    <div data="card header" className="CharacterCard__ImgWrapper">
+                        <div className="card-image">
+                            <img src="https://rickandmortyapi.com/api/character/avatar/65.jpeg"
+                                                         alt="Chris"/>
+                        </div>
+                        <div className="CharacterCard__Title">
+                            <h2 className="CharacterCard__Name">Chris</h2>
+                            <p className="CharacterCard__Description">id: 65 - created a year ago</p>
+                        </div>
+                    </div>
+                    <div data="card info" className="CharacterCard__InfoWrapper">
+                        <div className="CharacterCard__TextWrapper"><span>STATUS</span><p>Alive</p>
+                        </div>
+                        <div className="CharacterCard__TextWrapper"><span>SPECIES</span><p>Humanoid,
+                            Microverse inhabitant</p></div>
+                        <div className="CharacterCard__TextWrapper"><span>GENDER</span><p>Male</p>
+                        </div>
+                        <div className="CharacterCard__TextWrapper"><span>ORIGIN</span><p>Rick's
+                            Battery Microverse</p></div>
+                        <div className="CharacterCard__TextWrapper"><span>LAST LOCATION</span>
+                            <p>Rick's Battery Microverse</p></div>
+                    </div>
+                </article>
+
+                <article className="CharacterCard__Wrapper">
+                    <div data="card header" className="CharacterCard__ImgWrapper">
+                        <div className="card-image">
+                            <img src="https://rickandmortyapi.com/api/character/avatar/65.jpeg"
+                                 alt="Chris"/>
+                        </div>
+                        <div className="CharacterCard__Title">
+                            <h2 className="CharacterCard__Name">Chris</h2>
+                            <p className="CharacterCard__Description">id: 65 - created a year ago</p>
+                        </div>
+                    </div>
+                    <div data="card info" className="CharacterCard__InfoWrapper">
+                        <div className="CharacterCard__TextWrapper"><span>STATUS</span><p>Alive</p>
+                        </div>
+                        <div className="CharacterCard__TextWrapper"><span>SPECIES</span><p>Humanoid,
+                            Microverse inhabitant</p></div>
+                        <div className="CharacterCard__TextWrapper"><span>GENDER</span><p>Male</p>
+                        </div>
+                        <div className="CharacterCard__TextWrapper"><span>ORIGIN</span><p>Rick's
+                            Battery Microverse</p></div>
+                        <div className="CharacterCard__TextWrapper"><span>LAST LOCATION</span>
+                            <p>Rick's Battery Microverse</p></div>
+                    </div>
+                </article>
+
+            </div>
+        </section>
     )
 };
 
